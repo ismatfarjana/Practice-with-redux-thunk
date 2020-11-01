64ea5f94e89af2777cfd01dc5fa757c328567bbc
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-11-02 05:09:09 +1100

    rules of reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,11 @@
 import { combineReducers } from "redux";
+import postsReducer from "./postsReducer";
 
 export default combineReducers({
-  replaceMe: () => "I am bluey"
+  posts: postsReducer
 });
+
+//rule of reducers
+//1. must return any value besides "undefined"
+//2. produce "state" or "data' to be used indise app using pervious state and the action
+//3. must not reac out of itself to decide what value toreturn( reduecers are pure)
