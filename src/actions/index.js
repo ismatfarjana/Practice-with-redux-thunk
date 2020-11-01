7fa4c12d1464ef6d4d69a754272669798eb21ece
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-11-02 04:41:47 +1100

    async function with redux thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,10 @@
 import jsonPlaceHolder from "../apis/jsonPlaceHolder";
 
+//fetchpost function returning another function that gets the object
 export const fetchPosts = () => {
-  return function() {
-    const response = jsonPlaceHolder.get("/posts");
-    return {
-      type: "FETCH_POSTS",
-      payload: response
-    };
+  return async dispatch => {
+    const response = await jsonPlaceHolder.get("/posts");
+
+    dispatch({ type: "FETCH_POSTS", payload: response });
   };
 };
